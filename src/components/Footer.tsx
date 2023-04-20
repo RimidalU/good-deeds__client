b24ebf9648b01c39e449af29155a17c470880f90
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-20 21:34:15 +0300

    feat: implement footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from "@/styles/Footer.module.scss";
+
+const Footer = () => {
+	return (
+		<footer className={styles.footer}>
+			<a href="https://github.com/RimidalU" target="_blank">
+				RimidalU &copy; 2023
+			</a>
+		</footer>
+	);
+};
+
+export default Footer;
