92eddb15bc3a40a74c119aff21c58e2b5eeb861d
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-20 22:36:14 +0300

    feat: implement header

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import styles from "@/styles/Header.module.scss";
+import Link from "next/link";
+
+export default function Header() {
+	const isAuth = true;
+
+	return (
+		<header className={styles.header}>
+			<h1 className={styles.header__title}>Good Deeds</h1>
+			<nav className={styles.header__nav}>
+				<ul className={styles.nav__set}>
+					<li className="nav__item">
+						<Link href="/" className="nav__link">
+							Home
+						</Link>
+					</li>
+					<li className="nav__item">
+						<Link href="/users" className="nav__link">
+							Users
+						</Link>
+					</li>
+				</ul>
+			</nav>
+
+			{isAuth ? (
+				<div className={styles.user__control}>
+					<a className={styles.user__link} href="#">
+						UserName
+					</a>
+					<a className={styles.user__button} href="#">
+						LogOut
+					</a>
+				</div>
+			) : (
+				<div className={styles.user__control}>
+					<a className={styles.user__link} href="#">
+						LogIn
+					</a>
+					<a className={styles.user__button} href="#">
+						Sign up
+					</a>
+				</div>
+			)}
+		</header>
+	);
+}
