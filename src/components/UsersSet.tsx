2f0778dfffc76bd85291d0c00178395ad3461d3a
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-21 16:16:14 +0300

    fead: implement base users sets section

--- src/components/UsersSet.tsx
+++ src/components/UsersSet.tsx
@@ -1,6 +1,7 @@
 import { IUser } from "@/interfaces/types";
 import styles from "@/styles/UsersSet.module.scss";
 import Link from "next/link";
+import Image from "next/image";
 
 interface IUsersSetProps {
 	users: IUser[];
@@ -8,17 +9,21 @@ interface IUsersSetProps {
 
 const UsersSet = ({ users }: IUsersSetProps) => {
 	return (
-		<ul className="user__set">
+		<ul className={styles.users__set}>
 			{users.length > 0 &&
 				users.map((user) => (
-					<li className="user__item" key={user._id}>
-						<Link href={`users/${user._id}`}>
-							<article className="deed__body">
-								<p className="deed__description">{user.nickName} </p>
-								<h3 className="deed__name">{user.name} </h3>
+					<Link href={`users/${user._id}`} key={user._id}>
+						<li className={styles.user__item}>
+							<article className={styles.user__body}>
+								<Image className={styles.user__avatar} src="/logo.svg" alt={user.name} width={80} height={80} />
+								<div className={styles.user__info}>
+									<h3 className={styles.user__description}>@{user.nickName} </h3>
+									<span>Name: </span>
+									<p className={styles.user__name}>{user.name}</p>
+								</div>
 							</article>
-						</Link>
-					</li>
+						</li>
+					</Link>
 				))}
 		</ul>
 	);
