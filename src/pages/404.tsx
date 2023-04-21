207b2ae535bd16f5c0a887257d4fcec92805a655
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-21 03:16:19 +0300

    feat: add css container

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,4 @@
-import styles from "@/styles/Home.module.css";
-import Link from "next/link";
-import { IUser } from "@/interfaces/types";
 import Head from "next/head";
-
-interface IUsersProps {
-	users: IUser[];
-}
 
 export default function NotFound() {
 	return (
@@ -13,9 +6,9 @@ export default function NotFound() {
 			<Head>
 				<title>404</title>
 			</Head>
-			<main className={styles.main}>
+			<div className="wrapper">
 				<h1>Not Found!</h1>
-			</main>
+			</div>
 		</>
 	);
 }
