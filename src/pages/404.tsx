0107eb09b1947ce27accc93baab9a500937b2e73
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-22 21:48:19 +0300

    feat: refactor 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,27 @@
 import Head from "next/head";
+import styles from "@/styles/NotFound.module.scss";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function NotFound() {
+const NotFound = () => {
+	const router = useRouter();
+
+	useEffect(() => {
+		setTimeout(() => {
+			router.push("/");
+		}, 1000);
+	}, [router]);
+
 	return (
 		<>
 			<Head>
 				<title>404</title>
 			</Head>
 			<div className="wrapper">
-				<h1>Not Found!</h1>
+				<h1 className={styles.title}>Not Found!</h1>
 			</div>
 		</>
 	);
-}
+};
+
+export default NotFound;
