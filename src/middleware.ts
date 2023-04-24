6ae05b367dc1b48ed36e2bd9fa1a56cfe336aaf7
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-24 22:05:26 +0300

    feat: implement base auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
+
+// This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+// let verify = request.cookies.get("loggedin");
+// console.log(verify);
+
+// 	return NextResponse.redirect(new URL("/login", request.url));
+// }
+
+// See "Matching Paths" below to learn more
+export const config = {
+	matcher: ["/", "/users/:path*", "/deeds/:path*"],
+};
